d9cf18c2a2d600a80423cf91b60eeb69e9c84b1c
Hy-ta <hyuuga.1.29.59@gamil.com>
2021-01-16 11:21:48 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/{any}', function(){
-    return view('App');
-})->where('any', '.*');
+// Route::get('/{any}', function(){
+//     return view('App');
+// })->where('any', '.*');
+
+// Route::get('/{any}', function(){
+//     return view('app');
+// })->where('any', '.*');
 
